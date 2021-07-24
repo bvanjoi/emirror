d3a49e8c3fe129ffe0b010db2d80f89044079d7f
bvanjoi <610376039@qq.com>
2021-07-24 20:02:56 +0800

    docs(plugin/text): comment about nodeSpec.inline

--- packages/emirror__plugin-text/src/index.ts
+++ packages/emirror__plugin-text/src/index.ts
@@ -1,5 +1,8 @@
 import { Node } from '@emirror/core-structure';
 
+/**
+ * Text Node, must be exists in the Schema.nodes.
+ */
 class Text extends Node {
   get name() {
     return 'text' as const;
@@ -8,6 +11,9 @@ class Text extends Node {
   createNodeSpec() {
     return {
       group: 'inline',
+      // This filed is not required, because ProseMirror had
+      // treated it individually: [https://github.com/ProseMirror/prosemirror-model/blob/5ff774fa853dd63ecf30e42ff487b111b3c1c5fd/src/schema.js#L80].
+      // But I suggest write it down, just for clarity.
       inline: true,
     };
   }
